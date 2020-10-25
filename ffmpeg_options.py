654f99785ace285f13ad5c453335c02090f7372d
Matt Robinson <mrmcrrobinson@gmail.com>
2020-10-25 23:02:31 +0000

    [Added] - Encoding method comments.

--- ffmpeg_options.py
+++ ffmpeg_options.py
@@ -1,3 +1,7 @@
 general = "ffmpeg -f gdigrab -framerate 20 -offset_x"
-amd = "hevc_amf"
-nvidia = "hevc_nvenc"
+
+# Encoding method:
+    # amd: hevc_amf 
+    # nvidia: hevc_nvenc
+
+encoder = "hevc_nvenc"
